aaf13ca2208bcf522f005c64769c34bc8e8ee9f4
ledgku <ledgku@gmail.com>
2018-09-05 20:37:49 +0900

    [추가] GPS,IP 템플릿

--- dev/urls.py
+++ dev/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from dev.views import Main
+from dev.views import FindMyIp,FindMyGps
 
 app_name = 'dev'
 urlpatterns = [
-    path('', Main.as_view(), name = 'index'),
+    # path('', Main.as_view(), name = 'index'),
+    path('findmyip', FindMyIp.as_view(), name = 'findmyip'),
+    path('findmygps', FindMyGps.as_view(), name = 'findmygps'),
 ]
--- dev/views.py
+++ dev/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import View
 
 # Create your views here.
-class Main(View):
+class FindMyIp(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'base.html', {})
+        return render(request, 'dev/findmyip.html', {})
+
+
+class FindMyGps(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'dev/findmygps.html', {})
