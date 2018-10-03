1d39f3658f97509ce152fe2fe85bf4478e6566a1
nayak <wjdduq914@gmail.com>
2018-10-03 15:45:42 +0900

    추가. 접속자 ip 확인

--- dev/views.py
+++ dev/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+
 # Create your views here.
 class FindMyIp(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'dev/findmyip.html', {})
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return render(request, 'dev/findmyip.html', {
+            'ip': ip})
 
 
 class FindMyGps(View):
